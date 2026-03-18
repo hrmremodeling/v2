0c130cb89711a797dffc51a38963387923f4b788
hrmremodeling <hrm.remodeling@gmail.com>
2026-03-17 22:28:02 -0400

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import {
   Bath,
   Building2,
