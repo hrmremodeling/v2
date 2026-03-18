23006f655bb22e59e8c558dfa2187c267c4814c6
hrmremodeling <hrm.remodeling@gmail.com>
2026-03-17 22:58:07 -0400

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { useEffect } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
+import { Badge } from "@/components/ui/badge";;
 import {
   Bath,
   Building2,
